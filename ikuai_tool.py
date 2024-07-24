b0d5be9509acf8fd1ad4116dd44c098bb5258886
Leevan <80508977+Leevan-Zhang@users.noreply.github.com>
2024-07-24 13:19:39 +0800

    Update ikuai_tool.py
    modify SYSSTAT FUN

--- ikuai_tool.py
+++ ikuai_tool.py
@@ -4,6 +4,8 @@ import json
 from hashlib import md5
 from base64 import standard_b64encode
 from requests import post
+from requests import exceptions
+import asyncio
 class ikuai():
     def __init__(self,ip,username,password,method="http",verify=False):
         # parameters setting
@@ -29,39 +31,55 @@ class ikuai():
             "remember_password":"false"
         })
         self.response = requests.request("POST", url, headers="", data=payload, verify=self.verify)
-        self.cookie =  self.response.headers["Set-Cookie"]+f"username={self.username}; login=1"
-        return self.response
-    def actioncall(self,func_name:str,param:dict):
+        if self.response.status_code==200:
+            sess_key = self.response.headers["Set-Cookie"].split(";")[0]
+            self.cookie =  sess_key+f"; username={self.username}; login=1"
+            return self.response
+        else:
+            print("Login failed ! HTTP CODE : ",self.response.status_code)
+            raise exceptions.HTTPError(self.response)
+    def actioncall(self,action:str,func_name:str,param:dict):
         if self.cookie:
             url = f"{self.method}://{self.host}/Action/call"
             payload = dumps({
-                "action":"show",
+                "action":action,
                 "func_name":func_name,
-                "param":dumps(param)
+                "param":param
             })
-            response = post(url,headers={"Cookie":self.cookie},data=payload)
-            
-            return response
-        else:
-            raise ValueError("Missed cookie, please use login() to get session cookie")
-    
-    def getvolumes(self):
+
+            with post(url,headers={"Cookie":self.cookie},data=payload,verify=self.verify) as response:
+                if response.status_code == 200:return response
+                else:
+                    print(response.text)
+                    raise exceptions.HTTPError(response)
+        else:raise ValueError("Missed cookie, please use login() to get session cookie")
+    def getsysstat(self):
         if self.cookie:
-            func_name = "monitor_lanip"
+            func_name = "sysstat"
+            action="show"
             param = {
-                "TYPE": "data,total",
-                "ORDER_BY":"ip_addr_int",
-                "orderType":"IP",
-                "limit":"0,20",
-                "ORDER":"",
+                "TYPE": "verinfo,cpu,memory,stream,cputemp",
             }
-            response =  self.actioncall(func_name=func_name,param=param)
-            volumeresp  = json.loads(response.text)
+            response =  self.actioncall(action=action,func_name=func_name,param=param)
+            # print(response.text,response)
+
             try:
-                self.volumesdata = volumeresp["Data"]["data"]
-                print(f"Loading volumes successful ! Result : {volumeresp['Result']} msg:{volumeresp['ErrMsg']}")
-            except:
-                print(volumeresp)
+                volumeresp  = json.loads(response.text)
+                cpuload = volumeresp["Data"]["cpu"][0]
+                memoryused = volumeresp["Data"]["memory"]["used"]
+                connectnum = volumeresp["Data"]["stream"]["connect_num"]
+                download = volumeresp["Data"]["stream"]["download"]
+                upload = volumeresp["Data"]["stream"]["upload"]
+                infos = {
+                    "cpuload":cpuload,
+                    "memoryused":memoryused,
+                    "connectnum":connectnum,
+                    "download":download,
+                    "upload":upload,
+                }
+                return infos
+            except Exception as e:
+                print(e)
         else:
             raise ValueError("Missed cookie, please use login() to get session cookie")
 
